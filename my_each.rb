834691e390c4cdca0ae054319b03c8b2a137a421
oussama kahloul <oussama.kahloul.1996@gmail.com>
2017-03-16 18:51:45 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(baaa) # put argument(s) here
+  doo = 0
+    while doo < baaa.size
+      yield(baaa[doo])
+      doo = doo + 1
+    end
+      baaa 
   # code here
 end
+ 
