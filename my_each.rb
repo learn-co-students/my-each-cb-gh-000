589a6cf5237a9137fb974f1be61b64e701802e17
Rachel Thompson <thompr@pba.edu>
2017-08-15 01:03:28 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(fridge)
+  if block_given?
+    i = 0
+
+    while i < fridge.length
+      yield(fridge[i])
+      i += 1
+    end
+
+    fridge
+  else
+    return "The fridge is empty!"
+  end
+end
+
+def food(fridge = ["Tacos", "Burgers", "Pizza"]) food(fridge) do |i|
+    "I love #{i}"
+  end
 end
