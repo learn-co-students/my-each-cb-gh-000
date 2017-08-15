75138772222b704d83a03c62559e248400a0d49c
Andrei Lyskov <andreilyskov@gmail.com>
2017-08-15 20:40:08 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  counter = 0
+  if array.size == 0
+    return []
+  end
+  while array.size - 1 < counter
+    yield array[counter]
+    counter += 1
+  end
 end
