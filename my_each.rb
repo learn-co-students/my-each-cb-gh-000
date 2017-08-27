021f3463f3672e7fad2ea9c9b28f1e51c00439d9
Pablo Almancio Gargallo <al315490@uji.es>
2017-08-27 16:27:51 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if block_given?
+    count = 0
+
+    while count < collection.length
+      yield collection[count]
+      count += 1
+    end
+
+    collection
+  else
+    "nothing here"
+  end
+end
+
+my_each(['hi', 'hello', 'bye', 'goodbye']) do |i|
+  "#{i}"
 end
