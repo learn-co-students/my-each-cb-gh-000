d84a40e8d359f832b64ae072817cc4cbbc5efd43
Ertugrul Dal <ertugrul.dal@ogr.sakarya.edu.tr>
2018-07-09 01:44:12 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
+def my_each(collection)# put argument(s) here
   # code here
+
+  counter = 0
+
+  while counter < collection.length
+
+    yield collection[counter]
+    counter += 1
+
+  end
+  collection
+end
+
+
+['arel', 'jon', 'logan', 'spencer'].my_each do |i|
+  puts i
 end
