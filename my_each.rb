371c03afbb609c254ed140d6ffd7854eed9a69e1
Quinn Dizon <qdizon@gmail.com>
2018-07-17 19:46:30 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  if block_given?
+    i = 0
+
+    while i < arr.length
+      yield(arr[i])
+      i += 1
+    end
+    return arr
+  else
+
+  end
 end
+
+# collection = [1, 2, 3, 4]
+#
+# my_each(collection) do |element|
+#   element
+# end
