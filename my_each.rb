6a9552c4903202f3d6f8de01aba00fc50ca1d4d7
Jakob Baumgartner <jakob.baumgartner@hotmail.com>
2018-10-15 10:30:20 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each (array)# put argument(s) here
   # code here
+  leng = array.length
+  count = 0
+  while count < leng
+    yield(array[count])
+    count+=1
+  end
+  array
 end
