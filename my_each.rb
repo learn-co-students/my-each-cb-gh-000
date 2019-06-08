056fc128ac24997106c4a940696c4be657a542ef
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-08 14:28:44 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  i = 0
+  while i < array.size
+    if block_given?
+      yield(array[i])
+    end
+    i +=1
 end
