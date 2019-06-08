80dccab0e9e822c70d18f20a2740a05db570aa24
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-08 14:26:24 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,8 +2,7 @@ def my_each(array) # put argument(s) here
   # code here
   i = 0
   while i < array.size
-    if block_given? 
+    if block_given?
       yield(array[i])
-    end 
-    
+    end
 end
