f1d30ae6eca1e4c35fb8f902f4f01485ebb0a72a
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-10 08:39:37 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,12 @@
-def my_each (array)# put argument(s) here
-  yield
+def my_each (array)
+  i = 0
+  while i < array.size
+    yield array[i]
+    i += 1
+  end
   array
 end
+
+collection = [1,2,3,4]
+
+my_each(collection) {|i| print collection[i]}
